4f24791848787f3b1cc4d19b0f4619ec48f8fd2f
jackhowe-bio <44025175+jackhowe-bio@users.noreply.github.com>
2023-02-14 13:42:45 +0100

    Removed N from this model

--- Model2.R
+++ Model2.R
@@ -22,19 +22,15 @@ mySIRS <- function(t,y,params) {
     dZ = v*Y - (b + gamma)*Z
     
     
-    dN = (a-b)*(X+Y+Z) - alpha*Y 
-    
-    dD = alpha*Y
-    
-    return(list(c(dX, dY, dZ, dN)))
+    return(list(c(dX, dY, dZ)))
   })
 }
 run_shiny(model = "SIR Model (Anderson & May)", 
           neweqns = mySIRS,
-          ics = c(X = 999, Y = 1, Z = 0, N = 1000),
+          ics = c(X = 999, Y = 1, Z = 0),
           parm0 = c(a = 0.014 , b = 0.01, beta = 0.0001, alpha = 0.029, gamma = 0.015, v = 0.044),
           parm_names = c("birth rate", "background death rate", "rate of infection","diseased induced mortality","loss of immunity", " recovery rate"),
           parm_min = c(a = 0 , b = 0, beta = 0, alpha = 0, gamma = 0, v = 0),
           parm_max = c(a = 0.1 , b = 0.1, beta = 0.01, alpha = 0.1, gamma = 0.1, v = 0.1),
           tmax = 1000, 
-          values = c('cadetblue4','darkred','darkorchid1', 'grey'))
+          values = c('cadetblue4','darkred','darkorchid1'))
